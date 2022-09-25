febd344ddac1c601c5abe1ebbde735dee313c0e6
Stephen Sivetts <104328106+ss3359@users.noreply.github.com>
2022-09-24 21:27:53 -0600

    Latest Commit

--- MechaCar_Statistical_Analysis/MechaCarChallenge.R
+++ MechaCar_Statistical_Analysis/MechaCarChallenge.R
@@ -1,4 +1,14 @@
 library(dplyr)
 data_file = read.csv("MechaCar_mpg.csv")
-ln_reg_line = lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD+mpg, data=data_file)
+ln_reg_line = lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=data_file)
 summary(ln_reg_line)
+print(ln_reg_line)
+cor(ln_reg_line)
+
+data_file_two <- read.csv("Suspension_Coil.csv")
+total_summary <- data_file_two %>% summarize(mean(PSI), median(PSI),var(PSI), sd(PSI))
+print(total_summary)
+
+total_summary_df <- data_frame(total_summary)
+lot_summary <- data_file_two %>% group_by(Manufacturing_Lot) %>% summarize(mean(PSI), median(PSI),var(PSI), sd(PSI))
+lot_summary_df <- data.frame(lot_summary)
